ba8526d1893bb981ce25665c3045444b7ca3a23f
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-24 20:12:14 +0000

    add random yes/no

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import gradio as gr
+import random as rand
 
 def echo(message, history):
     print(message)
     print(history)
-    return message
+    num = rand.randint(0,1)
+    if num >= 0.5:
+        output = "Yes"
+    else 
+        output = "No"
+    return output
 
 chatbot = gr.ChatInterface(echo, type='messages')
 chatbot.launch()
