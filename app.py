ca92be1298c1c2211f8d9ab53ddd804d73c0e499
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-24 22:49:40 +0000

    fix syntax

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ def echo(message, history):
     list = ["Yes", "Yes, definitely", "Most likely", "No", "My sources say no", "Ask again later", "Don't count on it", "Very doubtful", "Outlook not so good"]
     return rand.choice(list)
 
-chatbot = gr.ChatInterface(echo, type='messages', examples=["Will it rain tomorrow?", "Will I enjoy my dinner?", "Will I be successful?"], title="8Oracle"))
+chatbot = gr.ChatInterface(echo, type='messages', examples=["Will it rain tomorrow?", "Will I enjoy my dinner?", "Will I be successful?"], title="8Oracle")
 chatbot.launch()
