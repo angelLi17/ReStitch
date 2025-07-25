d1df364ae1ddc726caa3251997c18af1eb55bd95
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-25 16:17:18 +0000

    hi

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import gradio as gr
 import random as rand
 
-def echo(message, history):
+def magic8ball(message, history):
     list = ["Yes", "Yes, definitely", "Most likely", "No", "My sources say no", "Ask again later", "Don't count on it", "Very doubtful", "Outlook not so good"]
     return rand.choice(list)
 
-chatbot = gr.ChatInterface(echo, type='messages', examples=["Will it rain tomorrow?", "Will I enjoy my dinner?", "Will I be successful?"], title="8Oracle")
+chatbot = gr.ChatInterface(magic8ball, type='messages', examples=["Will it rain tomorrow?", "Will I enjoy my dinner?", "Will I be successful?"], title="8Oracle")
 chatbot.launch()
