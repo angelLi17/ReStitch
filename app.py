9bc082e720d9798cd7a1f0b255c545b10f098033
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-25 17:22:40 +0000

    convert to generative reply

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import gradio as gr
-import random as rand
+from huggingface_hub import InferenceClient
 
-def magic8ball(message, history):
-    list = ["Yes", "Yes, definitely", "Most likely", "No", "My sources say no", "Ask again later", "Don't count on it", "Very doubtful", "Outlook not so good"]
-    return rand.choice(list)
+client = InferenceClient("HuggingFaceH4/zephr-7b-beta")
 
-chatbot = gr.ChatInterface(magic8ball, type='messages', examples=["Will it rain tomorrow?", "Will I enjoy my dinner?", "Will I be successful?"], title="8Oracle", description="I see your future...", theme=gr.themes.Glass())
+def respond(message, history):
+    messages = [{"role": "system", "content": "You are a friendly chatbot"}]
+    
+    if history:
+        messages.extend(history)
+
+    messages.append({"role": "user", "content": message})
+
+    response = client.chat_completion(
+        messages,
+        max_tokens=100,
+        temperature=0.2
+    ) 
+    
+    return response['choices'][0]['message']['content'].strip()
+
+chatbot = gr.ChatInterface(respond, type='messages', examples=["Will it rain tomorrow?", "Will I enjoy my dinner?", "Will I be successful?"], title="8Oracle", description="I see your future...", theme=gr.themes.Glass())
 chatbot.launch()
