de20f911de592d53b22e11c24466309607ff6b91
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-29 20:11:06 +0000

    Change theme

--- app.py
+++ app.py
@@ -147,5 +147,5 @@ def respond(message, history):
         response += token
         yield response
 
-chatbot = gr.ChatInterface(respond, type='messages', examples=["How do I repurpose my shirt?", "Is this good for the environment?", "Can you tell what to do with my old pants?"], title="ReStitch", description="Are you out of closet space? Is your closet filled with clothes you never use? Worry not, our chatbot is designed to give you trendy and creative ideas to make something new out of the old.", theme=gr.themes.Glass())
+chatbot = gr.ChatInterface(respond, type='messages', examples=["How do I repurpose my shirt?", "Is this good for the environment?", "Can you tell what to do with my old pants?"], title="ReStitch", description="Are you out of closet space? Is your closet filled with clothes you never use? Worry not, our chatbot is designed to give you trendy and creative ideas to make something new out of the old.", theme='kioshi/brightly-colored')
 chatbot.launch()
