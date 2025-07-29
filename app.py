511a3feed70c685af968a32f96960a7e604ec484
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-29 20:00:52 +0000

    Alter tokens amount and adjust chatbot personality

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 def respond(message, history):
     messages = [
         {"role": "system", 
-            "content": "You are a creative person who tells people how they can upcycle their clothing. You are very kind! Base your response on the provided context: {str_restitch_text}"
+            "content": "You are a creative person who tells people how they can upcycle their clothing in concise language. You are very kind! Base your response on the provided context: {str_restitch_text}"
         },
         {
             "role": "user",
@@ -139,7 +139,7 @@ def respond(message, history):
     
     for message in client.chat_completion(
         messages,
-        max_tokens=100,
+        max_tokens=150,
         temperature=0.2,
         stream=True
     ):
