08d4153cb5b779b963f7e446aa52db3315e36c12
Delilah Sophia-Siegel <delilahsophiasiegel@users.noreply.huggingface.co>
2025-07-30 21:18:55 +0000

    tried to debug

--- app.py
+++ app.py
@@ -124,13 +124,9 @@ def respond(message, history):
     
     messages = [
         {"role": "system", 
-            "content": f"You are a creative person who tells people how they can upcycle their clothing in concise language. Limit responses to 150 words and always end in a complete sentence. You are very kind! Base your response on the provided context: {str_restitch_chunks}"
+        "content": f"You are a creative person who tells people how they can upcycle their clothing in concise language. Limit responses to 150 words and always end in a complete sentence. You are very kind! Base your response on the provided context: {str_restitch_chunks}"
         },
-        {
-            "role": "user",
-            "content": (
-                f"Question: {message}"
-            )
+        {"role": "user", "content": f"Question: {message}"
         }
     ]
     
