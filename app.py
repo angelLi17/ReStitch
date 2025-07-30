3728f0d6508508456a762d5dcbba5d416f91ab5f
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-30 22:29:35 +0000

    Format include whitespace and add template for tomorrow and delete section

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import gradio as gr
 from huggingface_hub import InferenceClient
 
+
+#__________________________________________
 #STEP 1 FROM SEMANTIC SEARCH
 from sentence_transformers import SentenceTransformer
 import torch
+#__________________________________________
 
 
-
-
+#__________________________________________
 #STEP 2 FROM SEMANTIC SEARCH
 # Open the restitch.txt file in read mode with UTF-8 encoding
 with open("restitch.txt", "r", encoding="utf-8") as file:
@@ -15,9 +17,10 @@ with open("restitch.txt", "r", encoding="utf-8") as file:
   restitch_text = file.read()
 # jasmine: commenting this out bc it's a lot of text to print 
 # print(restitch_text)
+#__________________________________________
 
 
-
+#__________________________________________
 #STEP 3 FROM SEMANTIC SEARCH
 def preprocess_text(text):
   # Strip extra whitespace from the beginning and the end of the text
@@ -44,9 +47,10 @@ def preprocess_text(text):
 
 # Call the preprocess_text function and store the result in a cleaned_chunks variable
 cleaned_chunks = preprocess_text(restitch_text) # Complete this line
+#__________________________________________
 
 
-
+#__________________________________________
 #STEP 4 FROM SEMANTIC SEARCH
 # Load the pre-trained embedding model that converts text to vectors
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -68,8 +72,10 @@ def create_embeddings(text_chunks):
 chunk_embeddings = create_embeddings(cleaned_chunks) # Complete this line
 #print(cleaned_chunks)
 #print(chunk_embeddings)
+#__________________________________________
 
 
+#__________________________________________
 #STEP 5 FROM SEMANTIC SEARCH
 # Define a function to find the most relevant text chunks for a given query, chunk_embeddings, and text_chunks
 def get_top_chunks(query, chunk_embeddings, text_chunks):
@@ -105,14 +111,17 @@ def get_top_chunks(query, chunk_embeddings, text_chunks):
   # Return the list of most relevant chunks
   return top_chunks
 
-
+    
+#__________________________________________
 #STEP 6 FROM SEMANTIC SEARCH
 # Call the get_top_chunks function with the original query
 # top_results = get_top_chunks("How does water get into the sky", chunk_embeddings, cleaned_chunks) # Complete this line
 # Print the top results
 # print(top_results)
+#__________________________________________
 
 
+#__________________________________________
 client = InferenceClient("Qwen/Qwen2.5-72B-Instruct")
 
 desc = "ReStitch is a tool that aids in upcycling old clothes that have been sitting in your closet, untouched, for years." # update this
@@ -130,7 +139,10 @@ custom_theme = gr.themes.Ocean(
     font=[gr.themes.GoogleFont("Intel One Mono"), "serif"],
     font_mono=[gr.themes.GoogleFont("Playwrite Magyarország"), "cursive"]
 )
+#__________________________________________
 
+
+#__________________________________________
 def respond(message, history):
     response = ""
     best_restitch_chunks = get_top_chunks(message, chunk_embeddings, cleaned_chunks) # Complete this line
@@ -165,6 +177,8 @@ def respond(message, history):
 # chatbot = gr.ChatInterface(
 #     fn=respond, type='messages', examples=["How do I repurpose my shirt?", "Is this good for the environment?", "Can you tell what to do with my old pants?"], title="ReStitch", description="Are you out of closet space? Is your closet filled with clothes you never use? Worry not, our chatbot is designed to give you trendy and creative ideas to make something new out of the old.", theme='kioshi/brightly-colored')
 # chatbot.launch()
+#__________________________________________
+
 
 #__________________________________________
 with gr.Blocks(theme=custom_theme) as ReStitch:
@@ -180,8 +194,6 @@ with gr.Blocks(theme=custom_theme) as ReStitch:
                 gr.Markdown("ReStitch")
             with gr.Row():
                 gr.Markdown(tagline)
-    with gr.Row(scale=1):
-        gr.Markdown(desc)
     with gr.Row(scale=3):
         with gr.Column(scale=4):
             gr.ChatInterface(
@@ -193,4 +205,16 @@ with gr.Blocks(theme=custom_theme) as ReStitch:
             )
         with gr.Column(scale=1):
             gr.CheckboxGroup(["Beginner", "Intermediate", "Advanced"], label="What is your crafting skill level? Skills like sewing and tailoring.")
+    
+#__________________________________________
+#ADD SONG PLAYLIST HERE, EXAMPLE IMAGES, AND LINKS TO RESOURCES
+    # with gr.Row(scale=1):
+        # with gr.Column(scale=1):
+            #song playlist square shape
+        # with gr.Column(scale=4)
+            #example image canva here
+    # with gr.Row(scale=1)
+        # resources links here
+#__________________________________________        
+    
 ReStitch.launch()
