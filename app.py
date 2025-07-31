82ed783f0363dffaed397c253d4b2f197b6be5e8
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-31 18:40:37 +0000

    Add comma

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ with gr.Blocks(theme=custom_theme) as ReStitch:
                 examples=["How do I repurpose my shirt?", "Is this good for the environment?", "Can you tell what to do with my old pants?"], 
                 title="ReStitch", 
                 additional_inputs = [level],
-                examples=[["Advanced", "Beginner", "Intermediate"]]
+                examples=[["Advanced", "Beginner", "Intermediate"],]
                 description="Are you out of closet space? Is your closet filled with clothes you never use? Worry not, our chatbot is designed to give you trendy and creative ideas to make something new out of the old.", 
             )
     
