e84536b6b91e91702f31627b4d8eaad9eec610eb
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-31 18:29:26 +0000

    add input

--- app.py
+++ app.py
@@ -181,6 +181,7 @@ def respond(message, history, level, test):
 
 
 #__________________________________________
+test="test"
 with gr.Blocks(theme=custom_theme) as ReStitch:
     with gr.Row(scale=1):
         gr.Image(
@@ -213,7 +214,7 @@ with gr.Blocks(theme=custom_theme) as ReStitch:
                 type='messages', 
                 examples=["How do I repurpose my shirt?", "Is this good for the environment?", "Can you tell what to do with my old pants?"], 
                 title="ReStitch", 
-                additional_inputs = [level, "temp"],
+                additional_inputs = [level, test],
                 description="Are you out of closet space? Is your closet filled with clothes you never use? Worry not, our chatbot is designed to give you trendy and creative ideas to make something new out of the old.", 
             )
     
