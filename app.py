b410176779ae54c177d04ac1e616ac1f9722585d
Angel Li <angel-li@users.noreply.huggingface.co>
2025-07-31 18:21:52 +0000

    syntax comma

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ with gr.Blocks(theme=custom_theme) as ReStitch:
                 type='messages', 
                 examples=["How do I repurpose my shirt?", "Is this good for the environment?", "Can you tell what to do with my old pants?"], 
                 title="ReStitch", 
-                additional_inputs = [level]
+                additional_inputs = [level],
                 description="Are you out of closet space? Is your closet filled with clothes you never use? Worry not, our chatbot is designed to give you trendy and creative ideas to make something new out of the old.", 
             )
     
